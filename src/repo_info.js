60bd97412648d71a3f2efa4871b34aa2ac70435c
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-07-13 13:20:09 -0400

    Grabbing repos from your repositories and the news feed and storing there api data.

--- src/repo_info.js
+++ src/repo_info.js
@@ -1,27 +1,31 @@
 var RepoInfo = (function() {
-  var current_watched = $('.repos.watching li').not('.private'), 
-      stored_watched = loadStoredWatched(),
+  var current,
+      current_watched = $('.repos.watching li').not('.private').find('b > a'),
+      current_owned = $('#repo_listing li.public b > a'),
+      current_feed = $('div.alert.watch_started div.title > a:nth-child(3), div.alert.push div.title > a:nth-child(3)'),
+      stored = loadStoredWatched(),
       api_path = '/api/v2/json/repos/show/',
       finished_loading = false,
       on_finished_loading = [];
   
   function init() {
+    current = $.merge($.merge(current_watched, current_owned), current_feed);
     var repos = [], key
-    for(var i = 0; i < current_watched.length; i++) {
-      key = $(current_watched[i]).find('b > a').attr('href').replace(/^\/(.*)\/tree/, '$1');
-      if(stored_watched[key] === undefined) {
+    for(var i = 0; i < current.length; i++) {
+      key = $(current[i]).attr('href').replace(/(?:^\/|http:\/\/github.com\/)(.*)\/tree/, '$1');
+      if(stored[key] === undefined) {
         repos.push(key);
       }
     }
-    getAndStoreReposData(repos)
+    getAndStoreReposData(repos)      
   }
   
   function loadStoredWatched() {
-    return (JSON.parse(localStorage.getItem('watched_repositories')) || {})
+    return JSON.parse(localStorage.getItem('repositories') || "{}" )      
   }
   
   function saveStoredWatched() {
-    localStorage.setItem('watched_repositories', JSON.stringify(stored_watched));
+    localStorage.setItem('repositories', JSON.stringify(stored));
   }
   
   function getAndStoreReposData(repos) {
@@ -43,7 +47,7 @@ var RepoInfo = (function() {
   }
   
   function storeRepoJSON(repo, json) {
-    stored_watched[repo] = json;
+    stored[repo] = json;
     saveStoredWatched();
   }
     
