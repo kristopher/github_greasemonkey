d8ecdaae13d4e8ef334c63e01fd651ad3d04bbcf
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-07-13 13:21:52 -0400

    updating the dist.

--- dist/github_greasemonkey.user.js
+++ dist/github_greasemonkey.user.js
@@ -397,29 +397,39 @@ start = new Date()
 new Analyze.Repository();
 console.debug('Analyze.Repository: ' + (new Date - start));
 var RepoInfo = (function() {
-  var current_watched = $('.repos.watching li').not('.private'),
-      stored_watched = loadStoredWatched(),
+  var current,
+      current_watched = $('.repos.watching li').not('.private').find('b > a'),
+      current_owned = $('#repo_listing li.public b > a'),
+      current_feed = $('div.alert.watch_started div.title > a:nth-child(3), div.alert.push div.title > a:nth-child(3)'),
+      stored = loadStoredWatched(),
       api_path = '/api/v2/json/repos/show/',
       finished_loading = false,
       on_finished_loading = [];
 
   function init() {
-    var repos = [], key
-    for(var i = 0; i < current_watched.length; i++) {
-      key = $(current_watched[i]).find('b > a').attr('href').replace(/^\/(.*)\/tree/, '$1');
-      if(stored_watched[key] === undefined) {
-        repos.push(key);
+    try {
+      current = $.merge($.merge(current_watched, current_owned), current_feed);
+      console.debug(current);
+      var repos = [], key
+      for(var i = 0; i < current.length; i++) {
+        key = $(current[i]).attr('href').replace(/(?:^\/|http:\/\/github.com\/)(.*)\/tree/, '$1');
+        console.debug(key);
+        if(stored[key] === undefined) {
+          repos.push(key);
+        }
       }
+      getAndStoreReposData(repos)
+    } catch(e) {
+      console.error(e.message);
     }
-    getAndStoreReposData(repos)
   }
 
   function loadStoredWatched() {
-    return (JSON.parse(localStorage.getItem('watched_repositories')) || {})
+    return JSON.parse(localStorage.getItem('repositories') || "{}" )
   }
 
   function saveStoredWatched() {
-    localStorage.setItem('watched_repositories', JSON.stringify(stored_watched));
+    localStorage.setItem('repositories', JSON.stringify(stored));
   }
 
   function getAndStoreReposData(repos) {
@@ -441,7 +451,7 @@ var RepoInfo = (function() {
   }
 
   function storeRepoJSON(repo, json) {
-    stored_watched[repo] = json;
+    stored[repo] = json;
     saveStoredWatched();
   }
 
