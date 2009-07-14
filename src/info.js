09407f20530d355629fc517bdafe02461bb3ab10
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-07-14 15:06:43 -0400

    Updating the RepoSearch description and search data if the repositories are updated.

--- src/info.js
+++ src/info.js
@@ -4,6 +4,11 @@ var Info = function() {
 
 RepoInfo.onFinishedLoading(function() {
   UserInfo.init();
+  if (this.updated) {
+    JQuery.each(RepoSearch.instances, function() {
+      this.updateStoreRepositories();
+    })
+  }
 })  
 
 UserInfo.onFinishedLoading(function() {
