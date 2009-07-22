8e2a60562ff7ccb376324d2b5e06d260afebdc84
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-07-22 17:50:56 -0400

    Updating the dist.

--- dist/github_greasemonkey.user.js
+++ dist/github_greasemonkey.user.js
@@ -1087,11 +1087,9 @@ var Info = (function() {
 
 Info.init();
 
-RepoInfo.update();
-
 RepoInfo.onFinishedLoading(function() {
   UserInfo.update();
 });
 
-UserInfo.onFinishedLoading(function() {
-});
+RepoInfo.update();
+
